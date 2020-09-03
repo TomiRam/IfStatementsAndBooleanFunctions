4c21a232050845f2096bf3e2b194a7fe8445954d
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2020-09-03 10:41:45 +0300

    Add files via upload

---
+++ ConditionalLogicIfStatements.js
@@ -0,0 +1,17 @@
+function ourTrueorFalse(isItTrue) {
+	if (isItTrue) {
+
+		return "Yes, its true"
+
+	}	
+	return "No, its false" //can be true or false
+}
+
+function trueOrFalse(wasThatTrue){
+	if(wasThatTrue){
+		return "Yes, that was true"
+	}
+	return "No, that was false"
+}
+
+consle.log(trueOrFalse(true));
